cb1e275e226c6777a2017005b6596d3c4571f271
Navin Karkera <navin@disroot.org>
2022-12-30 15:53:35 +0530

    fix: mixin tests

--- tests/test_skill_tagging_mixin.py
+++ tests/test_skill_tagging_mixin.py
@@ -48,7 +48,7 @@ class SkillTaggingMixinTests(unittest.TestCase, TestCaseMixin):
         """
         self.tagging_mixin = get_tagging_mixin()
         self.assertTrue(hasattr(self.tagging_mixin, 'has_verified_tags'))
-        self.assertTrue(hasattr(self.tagging_mixin, 'fetch_tags'))
+        self.assertTrue(hasattr(self.tagging_mixin, 'fetch_skill_tags'))
         self.assertTrue(hasattr(self.tagging_mixin, 'verify_tags'))
 
     def test_fetch_tags(self, mock_get_api_client):
@@ -79,7 +79,7 @@ class SkillTaggingMixinTests(unittest.TestCase, TestCaseMixin):
             )
         )
         mock_get_api_client.return_value = api_client
-        resp = self.call_handler(self.FETCH_TAGS_HANDLER, data={})
+        resp = self.block.fetch_skill_tags()
         assert mock_get_api_client.call_count == 1
         assert api_client.get.call_count == 1
         expected_response = [
@@ -89,39 +89,17 @@ class SkillTaggingMixinTests(unittest.TestCase, TestCaseMixin):
         self.assertEqual(resp, expected_response)
 
     @patch('skill_tagging.skill_tagging_mixin.XBLOCK_SKILL_VERIFIED')
-    def test_verify_tags(self, mock_event, mock_get_api_client):
+    def test_verify_tags(self, mock_event, _):
         """
         Test that verify_tags method works as expected
         """
-        sample_output = {
-            "results": [{
-                "id": 1,
-                "skills": [
-                    {
-                        "id": 1,
-                        "name": "SKILL-0"
-                    },
-                    {
-                        "id": 6,
-                        "name": "SKILL-5"
-                    },
-                ],
-            }]
-        }
-        api_client = Mock(
-            get=Mock(
-                return_value=self._mock_response(
-                    status.HTTP_200_OK,
-                    sample_output
-                )
-            )
-        )
-        mock_get_api_client.return_value = api_client
         mock_event.return_value = Mock(send_event=Mock())
-        tags = ['SKILL-0', 'SKILL-5']
+        selected_tags = [1, 6]
+        ignored_tags = []
         self.assertFalse(self.block.has_verified_tags)
-        self.call_handler(self.VERIFY_TAGS_HANDLER, data=tags)
-        assert mock_get_api_client.call_count == 1
-        assert api_client.get.call_count == 1
+        self.call_handler(self.VERIFY_TAGS_HANDLER, data={
+            "verified_skills": selected_tags,
+            "ignored_skills": ignored_tags,
+        })
         assert mock_event.send_event.call_count == 1
         self.assertTrue(self.block.has_verified_tags)
