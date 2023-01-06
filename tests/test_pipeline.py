5c68c498ba74842ee0b267fdf919eb4b6c7e7240
Navin Karkera <navin@disroot.org>
2023-01-06 22:16:12 +0530

    feat: hook video skill verification block
    Introduces skill verification form in video blocks. It is displayed once
    the video ends specifically when `is-ended` class is applied to the
    video block. This is done by completion video-player service in
    edx-platform: https://github.com/openedx/edx-platform/blob/9f5c78e81e84fa4c345b5b06b49fa207943daf21/xmodule/js/src/video/03_video_player.js#L705-L708
    
    This commit also refactors verification form block for vertical blocks.

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -1,10 +1,11 @@
 """
 Tests for pipeline.py
 """
-from unittest.mock import Mock, patch
+from unittest.mock import patch
 
 from django.test import TestCase, override_settings
-from openedx_filters.learning.filters import VerticalBlockRenderCompleted
+from openedx_filters.learning.filters import VerticalBlockChildRenderStarted, VerticalBlockRenderCompleted
+from workbench.runtime import Fragment
 
 from test_utils import TestCaseMixin
 
@@ -28,7 +29,7 @@ class TestAddVerticalBlockSkillVerificationSection(TestCaseMixin, TestCase):
 
     def setUp(self) -> None:
         super().setUp()
-        self.original_fragement = Mock(content="<p>Some</p>")
+        self.original_fragement = Fragment(content="Some")
 
     @override_settings(
         SHOW_SKILL_VERIFICATION_PROBABILITY=0,
@@ -48,7 +49,7 @@ class TestAddVerticalBlockSkillVerificationSection(TestCaseMixin, TestCase):
     @override_settings(
         SHOW_SKILL_VERIFICATION_PROBABILITY=1,
     )
-    def test_pipeline_adds_edit_link_when_probability_set_to_one(self, mock_get_api_client):
+    def test_pipeline_adds_verification_form_when_probability_set_to_one(self, mock_get_api_client):
         """Check that verification div is added with skills."""
         mock_get_api_client.return_value = self.get_mock_api_response()
         _, fragment, _, _ = VerticalBlockRenderCompleted.run_filter(
@@ -58,3 +59,38 @@ class TestAddVerticalBlockSkillVerificationSection(TestCaseMixin, TestCase):
         self.assertIn(self.original_fragement.content, fragment.content)
         self.assertIn("SKILL-0", fragment.content)
         self.assertIn("SKILL-5", fragment.content)
+
+
+@patch('skill_tagging.skill_tagging_mixin.get_api_client')
+@override_settings(
+    OPEN_EDX_FILTERS_CONFIG={
+        "org.openedx.learning.vertical_block_child.render.started.v1": {
+            "fail_silently": False,
+            "pipeline": [
+                "skill_tagging.pipeline.AddVideoBlockSkillVerificationComponent"
+            ]
+        }
+    },
+    SHOW_SKILL_VERIFICATION_PROBABILITY=1,
+)
+class TestAddVideoBlockSkillVerificationComponent(TestCaseMixin, TestCase):
+    """
+    Testcase for the AddVerticalBlockSkillVerificationSection openedx-filters pipeline.
+    """
+
+    def setUp(self) -> None:
+        self.block_type = "video"
+        super().setUp()
+        self.original_fragement = Fragment(content="<div class='video-player'></div>")
+        self.block.fragment = self.original_fragement
+
+    def test_pipeline_adds_required_resources(self, mock_get_api_client):
+        """Check that pipeline adds required resources."""
+        mock_get_api_client.return_value = self.get_mock_api_response()
+        block, context = VerticalBlockChildRenderStarted.run_filter(block=self.block, context={})
+        fragment = block.student_view(context)
+
+        self.assertEqual(fragment.content, self.original_fragement.content)
+        self.assertEqual(len(fragment.resources), 3)
+        for resource in fragment.resources:
+            self.assertIn(resource.mimetype, ("text/css", "application/javascript"))
